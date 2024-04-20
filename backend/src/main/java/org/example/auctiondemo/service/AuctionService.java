e79cd6aeea707d3895511bb11a133729325a7792
MAHMUT CELIK <mahmut.382@hotmail.com>
2024-04-20 13:05:01 +0300

    BE-1

--- backend/src/main/java/org/example/auctiondemo/service/AuctionService.java
+++ backend/src/main/java/org/example/auctiondemo/service/AuctionService.java
@@ -3,13 +3,18 @@ package org.example.auctiondemo.service;
 import org.example.auctiondemo.model.*;
 import org.springframework.stereotype.Service;
 
+import java.nio.channels.NetworkChannel;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class AuctionService {
     final List<Auction> auctions = new ArrayList<>();
     final Map<Auction,HandleAuctionOffer> auctionHandlers = new HashMap<>();
+    ExecutorService executor = Executors.newCachedThreadPool();
+
 
     public List<Auction> getAuctions() {
         return auctions;
@@ -37,7 +42,7 @@ public class AuctionService {
         Auction auction = new Auction(auctionRequest.getId(),auctionRequest.getProduct(),auctionRequest.getUser(), LocalDateTime.now());
         auctions.add(auction);
         HandleAuctionOffer auctionOffer = new HandleAuctionOffer(auction);
-        auctionOffer.run();
+        executor.execute(auctionOffer);
         auctionHandlers.put(auction,auctionOffer);
     }
 }
