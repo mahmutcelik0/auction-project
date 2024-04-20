3b9e1489cd9c0fee17658d629d15e0ee10150eb7
sedat Korkmaz <68056590+sedatkimki@users.noreply.github.com>
2024-04-20 12:49:19 +0300

    auction cards

--- auction-client/src/screens/home/Home.tsx
+++ auction-client/src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { User } from "@/types";
+import { Auction, User } from "@/types";
 import { useSubscription, useStompClient } from "react-stomp-hooks";
 // import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -11,36 +11,68 @@ type Props = {
   user: User;
 };
 export const Home = ({ user }: Props) => {
-  const [lastMessage, setLastMessage] = useState("");
-  useSubscription("/join/user", (message) => setLastMessage(message.body));
+  const [lastMessage, setLastMessage] = useState("[]");
+  const [auctions, setAuctions] = useState([]);
+  useSubscription(`/auctions`, (message) =>
+    setLastMessage(message.body ? message.body : "[]")
+  );
 
-  // const stompClient = useStompClient();
+  const stompClient = useStompClient();
+
+  const createAuction = (data: Auction) => {
+    console.log(data);
+
+    stompClient?.publish({
+      destination: "/app/newAuction",
+      body: JSON.stringify({
+        ...data,
+      }),
+    });
+  };
+
   useEffect(() => {
-    console.log(user);
+    if (user.username !== "") {
+      stompClient?.publish({
+        destination: "/app/join/user",
+        body: JSON.stringify({
+          ...user,
+        }),
+      });
+    }
+  }, [stompClient, user]);
 
-    console.log(lastMessage);
-  }, [lastMessage, user]);
+  useEffect(() => {
+    console.log(JSON.parse(lastMessage));
+
+    setAuctions(JSON.parse(lastMessage));
+  }, [lastMessage]);
 
   return (
     <div>
       <div className="pt-6">
         <h2 className="text-3xl font-semibold">
           Welcome to auctions{" "}
-          <span className=" text-orange-500">{user.userName}</span>
+          <span className=" text-orange-500">{user.username}</span>
         </h2>
       </div>
       <Separator className="my-6" />
       <div className="pb-6 flex items-center justify-between">
         <h3 className="text-2xl font-semibold">Your auctions</h3>
         <span>
-          <AuctionCreateDialog user={user} />
+          <AuctionCreateDialog user={user} createAuction={createAuction} />
         </span>
       </div>
 
       <div className="grid gap-4 grid-cols-3 ">
-        <AuctionCard />
-        <AuctionCard />
-        <AuctionCard />
+        {auctions.map((auction: Auction) =>
+          auction.user.id === user.id ? (
+            <AuctionCard
+              key={auction.id}
+              auction={auction}
+              isBelongToUser={true}
+            />
+          ) : null
+        )}
       </div>
 
       <Separator className="my-6" />
@@ -50,9 +82,15 @@ export const Home = ({ user }: Props) => {
       </div>
 
       <div className="grid gap-4 grid-cols-3 ">
-        <AuctionCard />
-        <AuctionCard />
-        <AuctionCard />
+        {auctions.map((auction: Auction) =>
+          auction.user.id !== user.id ? (
+            <AuctionCard
+              key={auction.id}
+              auction={auction}
+              isBelongToUser={false}
+            />
+          ) : null
+        )}
       </div>
       {/* <Button
         onClick={() =>
