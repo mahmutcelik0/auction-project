0c9cea8fc1c4c42b1473e8ebf313b5030f0ea991
sedat Korkmaz <68056590+sedatkimki@users.noreply.github.com>
2024-04-20 11:36:32 +0300

    create auction dialog

---
+++ auction-client/src/screens/home/AuctionCreateDialog.tsx
@@ -0,0 +1,97 @@
+import { FC } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Auction, User } from "@/types";
+import { v4 as uuid } from "uuid";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+const auctionCreateSchema = z.object({
+  title: z.string().min(2).max(50),
+  basePrice: z.coerce.number().min(0.01, "Price must be greater than 0"),
+});
+
+type AuctionCreateDialogProps = {
+  user: User;
+};
+
+export const AuctionCreateDialog: FC<AuctionCreateDialogProps> = ({ user }) => {
+  const form = useForm<z.infer<typeof auctionCreateSchema>>({
+    resolver: zodResolver(auctionCreateSchema),
+    defaultValues: {
+      title: "",
+      basePrice: 0,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof auctionCreateSchema>) {
+    const auction: Auction = {
+      id: uuid(),
+      product: {
+        productName: values.title,
+        basePrice: values.basePrice,
+        currentPrice: values.basePrice,
+      },
+      createdByUser: user,
+    };
+    console.log(auction);
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create new auction</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Product Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Mona lisa" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="basePrice"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Base Price</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Mona lisa" type="number" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Create auction</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
